6cdb3634103f32ae08f3cd0186e61a181de62c0b
Ziggyyy92 <igor.stajic92@gmail.com>
2022-06-14 22:17:05 +0200

    Angular-reddit vezba
    Odradjena aplikacija iz pete lekcije

---
+++ angular-reddit/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-reddit';
+
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('IT255 - Veb sistemi 1', 'http://www.metropolitan.ac.rs', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+  
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+}
