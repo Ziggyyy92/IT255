f1b48d5dcfcb3b356e1b85e1a930a9877cd54036
Ziggyyy92 <igor.stajic92@gmail.com>
2022-06-26 20:26:44 +0200

    Zavrsena 9. vezba

---
+++ it255-v09/src/app/services/post.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Post } from '../models/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  private baseUrl = "https://jsonplaceholder.typicode.com/posts"
+
+  constructor(private _httpClient: HttpClient) { }
+
+  public getPosts(): Observable<Post[]> {
+    return this._httpClient.get(this.baseUrl).pipe(map((data: any) => data.map((item: any) => 
+    this._createPostFromObject(item))),
+    );
+  }
+
+  public getPost(id: number): Observable<Post> {
+    return this._httpClient.get(this.baseUrl + '/' + id).pipe(
+      map((data: any) => this._createPostFromObject(data)),
+    );
+  }
+
+  public deletePost(id: number): Observable<Post> {
+    return this._httpClient.delete(this.baseUrl + '/' + id).pipe(
+      map((data: any) => this._createPostFromObject(data)),
+    );
+  }
+
+  public createPost(post: Post): Observable<Post> {
+    return this._httpClient.post(this.baseUrl, post).pipe(
+      map((data: any) => this._createPostFromObject(data)),
+    );
+  }
+
+
+  private _createPostFromObject(item: any) {
+    return new Post(item.userId, item.title, item.body, item.id);
+  }
+}
